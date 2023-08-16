acd03aa4204cba2b68c57719c2c2a5d3e7b4edb0
kaitod-id <108166655+kaitod-id@users.noreply.github.com>
2023-08-16 21:48:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ load_dotenv() if not path.exists("local.env") else load_dotenv("local.env")
 
 
 class Config:
-    api_id = int(getenv("API_ID", "0"))
-    api_hash = getenv("API_HASH", "abc")
-    bot_token = getenv("BOT_TOKEN", "123:Abc")
-    log_channel = int(getenv("LOG_CHANNEL"))
-    fsub_chid = int(getenv("FORCESUB_CHANNEL"))
-    db_chid = int(getenv("DB_CHANNEL"))
-    blacklisted_channel = [int(x) for x in getenv("BLACKLISTED_CHANNEL", "123").split(",") if x is not None]
-    channel1 = int(getenv("CHANNEL_1"))
-    channel2 = int(getenv("CHANNEL_2"))
-    channel3 = int(getenv("CHANNEL_3"))
+    api_id = int(getenv("API_ID", "24465982"))
+    api_hash = getenv("API_HASH", "2b3131b7d3f6a42bd4ae1ba3b58c11c4")
+    bot_token = getenv("BOT_TOKEN", "6344639589:AAEIxPkMYUUr2K6PloxytU-CR-oeMYLeErU")
+    log_channel = int(getenv("LOG_CHANNEL", "-1001930361818"))
+    fsub_chid = int(getenv("FORCESUB_CHANNEL", "-1001825244023"))
+    db_chid = int(getenv("DB_CHANNEL", "-1001930361818"))
+    blacklisted_channel = [int(x) for x in getenv("BLACKLISTED_CHANNEL", "-1001965792519").split(",") if x is not None]
+    channel1 = int(getenv("CHANNEL_1", "-1001825244023"))
+    channel2 = int(getenv("CHANNEL_2", "-1001855788004"))
+    channel3 = int(getenv("CHANNEL_3", "-1001802473399"))
 
 
 config = Config()
